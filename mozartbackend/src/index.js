779ac6cd5caf51a517abdfa4b759f3e790ef8177
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-09 18:32:35 +0000

    first steps...

--- mozartbackend/src/index.js
+++ mozartbackend/src/index.js
@@ -1,11 +1,24 @@
+const _ = require('lodash');
 const express = require('express')
 const app = express()
-const port = 3001
+require('dotenv').config();
+var os = require('os');
 
-app.get('/api', (req, res) => {
-  console.log('Hello World /api');
-  res.send({api: "v1", version: "1.0.0"});
+
+const { PORT } = process.env;
+
+app.get('/api/cpu', (req, res) => {
+  console.log('---------- CPU Info ----------- ');
+  const sysinfo = {
+    cpu_model: os.cpus()[0].model,
+    logical_cpus: _.size(os.cpus()),
+    total_mem: Math.floor(os.totalmem() / 1024 / 1024),
+    free_mem: Math.floor(os.freemem() / 1024 / 1024),
+    hostname: os.hostname(),
+    network_interfaces: os.networkInterfaces(),
+    os_load_avg: os.loadavg(),
+  }
+  res.json(sysinfo);
 });
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`mozart_backend API listening on PORT ${PORT}!`))
--- mozarthome/src/App.js
+++ mozarthome/src/App.js
@@ -1,32 +1,94 @@
 import React, { Component } from 'react';
-import './App.css';
+import _ from 'lodash';
+import './App.scss';
 
 class App extends Component {
   state = {
-    info: 'nothing...',
+    cpuModel: 'getting info...',
+    hostname: 'getting hostname...',
   }
 
   componentDidMount() {
-    fetch('/api', {
+    return this.fetchSysInfo();
+  }
+
+  fetchSysInfo = async () => {
+    const sysInfo = await fetch('/api/cpu', {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
       }
     })
       .then(res => res.json())
-      .then(res => console.log(res));
+
+    return this.setState({
+      cpuModel: sysInfo.cpu_model,
+      freeMem: sysInfo.free_mem,
+      totalMem: sysInfo.total_mem,
+      hostname: sysInfo.hostname,
+      networkInterfaces: sysInfo.network_interfaces,
+      osLoadAvg: sysInfo.os_load_avg,
+      logicalCpus: sysInfo.logical_cpus
+    });
+  }
+
+  renderOSLoadAverage = () => {
+    const { osLoadAvg, logicalCpus } = this.state;
+    return (
+      <>
+        <h5>Logical CPUs: {logicalCpus}</h5>
+        <div style={{ fontSize: '80%' }}>
+          The load average is a measure of system activity, calculated by the operating system and expressed as a fractional number. As a rule of thumb, the load average should ideally be less than the number of logical CPUs in the system.
+        </div>
+        <div className="mt-2">
+        { _(osLoadAvg).map(item =>{
+          return (
+            <div key={_.uniqueId('loadAvgItem-')}>
+              { item }
+            </div>
+          );
+          }).value()
+        }
+        </div>
+      </>
+    );
+  }
+
+  renderNetworkInterfaces = () => {
+    const { networkInterfaces } = this.state;
+    return _(networkInterfaces).map((interfaceData, interfaceId) => {
+      return (
+        <span key={_.uniqueId('interface-')} className="text-danger">{ interfaceId }{" "}</span>
+      );
+    }).value();
   }
 
   render() {
     return (
-      <div className="App">
+      <div className="App text-center">
         <div className="my-2">
-          <h4>MOZART WEBSERVER</h4>
+          <h2>MOZART WEBSERVER</h2>
         </div>
-        <button className="btn btn-danger"
-          onClick={() => console.log('click click...')}>
-          Click Me!
-        </button>
+        <div>
+          CPU Model: { this.state.cpuModel }
+        </div>
+        <div>
+          Hostname: { this.state.hostname }
+        </div>
+        <div className="mt-2">
+          Total System Memory: { this.state.totalMem } MB
+        </div>
+        <div>
+          Free System Memory: { this.state.freeMem } MB
+        </div>
+        <div className="my-3 p-3">
+          <h3>OS Load Average</h3>
+          { this.renderOSLoadAverage() }
+        </div>
+        <div className="my-3 p-3">
+          <h3>Network Interfaces</h3>
+          { this.renderNetworkInterfaces() }
+        </div>
       </div>
     );
   }
