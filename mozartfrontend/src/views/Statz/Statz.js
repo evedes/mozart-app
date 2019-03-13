f046cc8fe4c13c5f41282f2c4dbd7f8a7636a2a6
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-13 11:55:07 +0000

    implemented Statz.scss

--- mozartfrontend/src/views/Statz/Statz.js
+++ mozartfrontend/src/views/Statz/Statz.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import SystemLoadAverageWidget from '../../widgets/SystemLoadAverageWidget';
 import StatzHeader from '../../widgets/StatzHeader';
 import NetworkInterfacesWidget from '../../widgets/NewtorkInterfacesWidget';
-
+import './Statz.scss';
 class Statz extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Statz">
         <StatzHeader />
         <SystemLoadAverageWidget height={200} width={600}/>
         <NetworkInterfacesWidget />
