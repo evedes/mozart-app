7507f3a715cb5aa3097b46adba62ee7aa6c44db1
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-13 11:53:16 +0000

    improved mozart header with silhouette

--- mozartfrontend/src/components/MozartHeader/MozartHeader.js
+++ mozartfrontend/src/components/MozartHeader/MozartHeader.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './MozartHeader.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import mozartSilhouette from '../../libs/imgs/mozart.png';
 
 class MozartHeader extends React.Component{
   render() {
     return(
       <div className="MozartHeader d-flex justify-content-start align-items-center">
-        <div className="mx-3">
-          MOZART APP
+        <div className="ml-3">
+          MOZART 
         </div>
+        <div className="mx-3 rounded">
+          <img src={mozartSilhouette} height="45" width="40" alt="mozart" />
+        </div>
+        <div className="ml-3">
+          METRICS
+        </div>
+        <div className="ml-auto mr-3">
+          <FontAwesomeIcon icon="hamburger" style={{ color: 'white', height: '45px' }} />
+        </div>
+
       </div>
     )
   }
