86efb24390c2132e654475416e4edab2797fc9b9
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-13 11:54:48 +0000

    passing props into MozartBox child components

--- mozartfrontend/src/components/MozartBox/MozartBox.js
+++ mozartfrontend/src/components/MozartBox/MozartBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 class MozartBox extends React.Component {
   render() {
     return (
-      <div className="m-3 p-2 shadow border">
+      <div props={this.props} className="m-2 p-2" style={{ border: '1px dashed gray' }}>
         { this.props.children }
       </div>
     )
