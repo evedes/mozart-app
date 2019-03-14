5ecc3a08dc5d694308f5ed768310d17f974a8357
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-14 22:46:08 +0000

    Statz view improvement

--- mozartfrontend/src/views/Statz/Statz.js
+++ mozartfrontend/src/views/Statz/Statz.js
@@ -3,12 +3,13 @@ import SystemLoadAverageWidget from '../../widgets/SystemLoadAverageWidget';
 import StatzHeader from '../../widgets/StatzHeader';
 import NetworkInterfacesWidget from '../../widgets/NewtorkInterfacesWidget';
 import './Statz.scss';
+
 class Statz extends Component {
   render() {
     return (
       <div className="Statz">
         <StatzHeader />
-        <SystemLoadAverageWidget height={200} width={600}/>
+        <SystemLoadAverageWidget height={250} />
         <NetworkInterfacesWidget />
       </div>
     );
