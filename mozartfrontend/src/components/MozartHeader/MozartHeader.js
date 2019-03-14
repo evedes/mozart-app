5c8c0759f408b60160d991c0cbba0ba654e92e3e
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-14 22:44:50 +0000

    refactored design, gradient color logo

--- mozartfrontend/src/components/MozartHeader/MozartHeader.js
+++ mozartfrontend/src/components/MozartHeader/MozartHeader.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './MozartHeader.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import mozartSilhouette from '../../libs/imgs/mozart.png';
+import mozartSilhouette from '../../libs/imgs/mozartfilled.png';
 
 class MozartHeader extends React.Component{
   render() {
     return(
       <div className="MozartHeader d-flex justify-content-start align-items-center">
         <div className="ml-3">
-          MOZART 
+          <img src={mozartSilhouette}
+            height="35"
+            width="auto"
+            alt="mozart"
+          />
         </div>
-        <div className="mx-3 rounded">
-          <img src={mozartSilhouette} height="45" width="40" alt="mozart" />
-        </div>
-        <div className="ml-3">
-          METRICS
+        <div className="ml-3 mt-2">
+          MOZART METRICS
         </div>
         <div className="ml-auto mr-3">
-          <FontAwesomeIcon icon="hamburger" style={{ color: 'white', height: '45px' }} />
+          <FontAwesomeIcon icon="hamburger" style={{ height: '35px' }} />
         </div>
 
       </div>
