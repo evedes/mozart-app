d257f7b0e3a39782f5f70bab9c3bdba793c66624
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-16 20:32:20 +0000

    transformed Statz view into a functional component

--- mozartfrontend/src/views/Statz/Statz.js
+++ mozartfrontend/src/views/Statz/Statz.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import SystemLoadAverageWidget from '../../widgets/SystemLoadAverageWidget';
+import React from 'react';
 import StatzHeader from '../../widgets/StatzHeader';
+import SystemLoadAverageWidget from '../../widgets/SystemLoadAverageWidget';
 import NetworkInterfacesWidget from '../../widgets/NewtorkInterfacesWidget';
 import './Statz.scss';
 
-class Statz extends Component {
-  render() {
-    return (
-      <div className="Statz">
-        <StatzHeader />
-        <NetworkInterfacesWidget height={250} width={300} />
-        <SystemLoadAverageWidget height={250} />
-        <NetworkInterfacesWidget height={250} width={300} />
-        <SystemLoadAverageWidget height={250} />
-      </div>
-    );
-  }
-}
+const Statz = () => (
+  <div className="Statz">
+    <StatzHeader />
+    <NetworkInterfacesWidget height={250} width={300} />
+    <SystemLoadAverageWidget height={250} />
+    <NetworkInterfacesWidget height={250} width={300} />
+    <SystemLoadAverageWidget height={250} />
+  </div>
+);
 
 export default Statz;
