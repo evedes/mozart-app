e71dcf4fdd8c2426435d8fd18ad793daadecc5f3
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-16 20:31:39 +0000

    added faSpinner to fa lib file

--- mozartfrontend/src/libs/fontAwesomeLib.js
+++ mozartfrontend/src/libs/fontAwesomeLib.js
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+import { faHamburger, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-export default library.add(faHamburger);
+export default library.add(faHamburger, faSpinner);
