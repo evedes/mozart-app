3a8e20bf9ee6b65464dceb0ec1a6c38e16c579c4
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-16 22:17:27 +0000

    added caret down to font awesome

--- mozartfrontend/src/libs/fontAwesomeLib.js
+++ mozartfrontend/src/libs/fontAwesomeLib.js
@@ -1,4 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHamburger, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHamburger,
+  faSpinner,
+  faCaretDown,
+} from '@fortawesome/free-solid-svg-icons';
 
-export default library.add(faHamburger, faSpinner);
+export default library.add(faHamburger, faSpinner, faCaretDown);
