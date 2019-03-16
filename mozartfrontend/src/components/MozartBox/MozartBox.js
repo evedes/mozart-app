923afa10371e4207dd037279682f34194430a452
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-16 20:30:43 +0000

    improve MozartBox to get style props

--- mozartfrontend/src/components/MozartBox/MozartBox.js
+++ mozartfrontend/src/components/MozartBox/MozartBox.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { object, array, func, oneOfType } from 'prop-types';
+import { object, string, array, func, oneOfType } from 'prop-types';
 import './MozartBox.scss';
 
 class MozartBox extends React.Component {
   render() {
-    const { children } = this.props;
+    const { children, style } = this.props;
     return (
-      <div props={this.props} className="MozartBox m-2 p-2 shadow">
+      <div
+        props={this.props}
+        style={style}
+        className="MozartBox m-2 p-2 shadow"
+      >
         {children}
       </div>
     );
@@ -15,6 +19,7 @@ class MozartBox extends React.Component {
 
 MozartBox.propTypes = {
   children: oneOfType([object, array, func]),
+  style: oneOfType([string, array, object]),
 };
 
 export default MozartBox;
