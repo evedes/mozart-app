9150a2f83c21004464fe8fc2fc46ad91a82b795f
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-17 00:30:13 +0000

    added StatzGrid route to implement react-grid-layout

--- mozartfrontend/src/routes.js
+++ mozartfrontend/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Statz from './views/Statz';
+import StatzGrid from './views/StatzGrid';
 import NoMatch from './views/NoMatch';
 import MozartHeader from './components/MozartHeader';
 
@@ -8,7 +9,8 @@ const Routes = () => (
   <div>
     <MozartHeader />
     <Switch>
-      <Route exact path="/" component={Statz} />
+      <Route exact path="/" component={StatzGrid} />
+      <Route exact path="/Statz" component={Statz} />
       <Route component={NoMatch} />
     </Switch>
   </div>
