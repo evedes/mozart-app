d910c721e38be41598d73a09a205faa0a200afad
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-17 00:31:46 +0000

    noMatch component: next step

--- mozartfrontend/src/views/NoMatch/NoMatch.js
+++ mozartfrontend/src/views/NoMatch/NoMatch.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { object } from 'prop-types';
+import MozartSpinner from '../../components/MozartSpinner';
+
+import './NoMatch.scss';
 
 class NoMatch extends React.Component {
+  componentDidMount() {
+    const { history } = this.props;
+    return setTimeout(() => history.push('/'), 2000);
+  }
+
   render() {
-    return <div>No Match</div>;
+    return (
+      <div
+        style={{ height: '100vh' }}
+        className="NoMatch d-flex flex-column justify-content-center align-items-center"
+      >
+        <h3>404 Unknown Route</h3>
+        <div className="m-3">
+          <MozartSpinner withoutBox size="3x" />
+        </div>
+      </div>
+    );
   }
 }
+
+NoMatch.propTypes = {
+  history: object,
+};
 
 export default NoMatch;
