5b7e43a2f9254d0c3f86fb2d2d49736d72895d8c
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-17 00:31:19 +0000

    improved mozart spinner api

--- mozartfrontend/src/components/MozartSpinner/MozartSpinner.js
+++ mozartfrontend/src/components/MozartSpinner/MozartSpinner.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import { string, bool } from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import MozartBox from '../MozartBox';
 
 class MozartSpinner extends React.Component {
+  static defaultProps = {
+    size: 'sm',
+    withoutBox: false,
+  };
+
   render() {
+    const { size, withoutBox } = this.props;
+
+    if (withoutBox) {
+      return (
+        <FontAwesomeIcon
+          style={{ color: 'red' }}
+          icon="spinner"
+          size={size}
+          spin
+        />
+      );
+    }
+
     return (
       <MozartBox style={{ color: 'red' }}>
-        <FontAwesomeIcon icon="spinner" spin />
+        <FontAwesomeIcon icon="spinner" size={size} spin />
       </MozartBox>
     );
   }
 }
+
+MozartSpinner.propTypes = {
+  size: string,
+  withoutBox: bool,
+};
 
 export default MozartSpinner;
