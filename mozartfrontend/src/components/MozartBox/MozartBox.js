cdf1b3e0a218d747bfc26cd0ed5fe19cd5d6c0b2
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-18 18:13:17 +0000

    improve MozartBox to be the most agnostic

--- mozartfrontend/src/components/MozartBox/MozartBox.js
+++ mozartfrontend/src/components/MozartBox/MozartBox.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { object, string, array, func, oneOfType } from 'prop-types';
+import { object, array, oneOfType } from 'prop-types';
 import './MozartBox.scss';
 
 class MozartBox extends React.Component {
   render() {
-    const { children, style } = this.props;
-    return (
-      <div
-        props={this.props}
-        style={style}
-        className="MozartBox m-2 p-2 shadow"
-      >
-        {children}
-      </div>
-    );
+    const { children } = this.props;
+    return <div className="MozartBox p-2 shadow">{children}</div>;
   }
 }
 
 MozartBox.propTypes = {
-  children: oneOfType([object, array, func]),
-  style: oneOfType([string, array, object]),
+  children: oneOfType([object, array]),
 };
 
 export default MozartBox;
