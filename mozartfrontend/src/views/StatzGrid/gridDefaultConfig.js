c503f0174e81a37c6db8fc5905569aa9d2c13204
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-21 19:29:04 +0000

    StatzHeader is now static

--- mozartfrontend/src/views/StatzGrid/gridDefaultConfig.js
+++ mozartfrontend/src/views/StatzGrid/gridDefaultConfig.js
@@ -1,16 +1,16 @@
 export const defaultLayouts = {
   lg: [
-    { i: 'StatzHeader', x: 0, y: 0, w: 3, h: 5, minH: 5 },
+    { i: 'StatzHeader', x: 0, y: 0, w: 3, h: 5, minH: 5, static: true },
     { i: 'NetworkInterfacesWidget', x: 0, y: 5, w: 3, h: 8, minH: 8 },
     { i: 'SystemLoadAverageWidget', x: 0, y: 13, w: 3, h: 8, minH: 8 },
   ],
   md: [
-    { i: 'StatzHeader', x: 0, y: 0, w: 2, h: 5, minH: 5 },
+    { i: 'StatzHeader', x: 0, y: 0, w: 2, h: 5, minH: 5, static: true },
     { i: 'NetworkInterfacesWidget', x: 0, y: 5, w: 2, h: 8, minH: 8 },
     { i: 'SystemLoadAverageWidget', x: 0, y: 13, w: 2, h: 8, minH: 8 },
   ],
   sm: [
-    { i: 'StatzHeader', x: 0, y: 0, w: 1, h: 5, minH: 5 },
+    { i: 'StatzHeader', x: 0, y: 0, w: 1, h: 5, minH: 5, static: true },
     { i: 'NetworkInterfacesWidget', x: 0, y: 5, w: 1, h: 8, minH: 8 },
     { i: 'SystemLoadAverageWidget', x: 0, y: 13, w: 5, h: 8, minH: 8 },
   ],
