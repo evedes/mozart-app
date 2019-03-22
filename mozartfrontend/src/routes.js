1c13e8b57874fd98c4afcee020ca4066579af457
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-22 01:32:30 +0000

    moved Router here

--- mozartfrontend/src/routes.js
+++ mozartfrontend/src/routes.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import StatzGrid from './views/StatzGrid';
 import NoMatch from './views/NoMatch';
 import MozartHeader from './components/MozartHeader';
 
 const Routes = () => (
-  <div>
+  <>
     <MozartHeader />
-    <Switch>
-      <Route exact path="/" component={StatzGrid} />
-      <Route component={NoMatch} />
-    </Switch>
-  </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={StatzGrid} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
+  </>
 );
 
 export default Routes;
