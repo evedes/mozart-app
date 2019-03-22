56b79d9d6c9a23a66324e32dd8604745aab65b67
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-22 01:32:10 +0000

    redux added

--- mozartfrontend/src/index.js
+++ mozartfrontend/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import './libs/fontAwesomeLib';
 
+import { chartingPeriod } from './reducers';
+
+const store = createStore(chartingPeriod);
+
 ReactDOM.render(
-  <Router>
+  <Provider store={store}>
     <Routes />
-  </Router>,
+  </Provider>,
   document.getElementById('root')
 );
 
