1ca6a240fbe1df745b0e414be43cb1af935db45a
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-24 11:42:37 +0000

    added storybook

---
+++ mozartfrontend/sourcecode/stories/eduardo.stories.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+// import { linkTo } from '@storybook/addon-links';
+
+storiesOf('Start Creating...', module).add('to Storybook', () => (
+  <div>Hello Batman</div>
+));
