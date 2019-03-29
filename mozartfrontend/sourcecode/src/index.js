b841ae0dad435da88b53b16f8bb143af28c09144
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-03-29 15:25:32 +0000

    reconfigured redux with combineReducers

--- mozartfrontend/sourcecode/src/index.js
+++ mozartfrontend/sourcecode/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
@@ -8,8 +8,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './libs/fontAwesomeLib';
 
 import mozartReducer from './reducers';
+import networkStatzReducer from './widgets/NetworkInterfacesWidget/reducers';
+import cpuSystemLoadAvgReducer from './widgets/SystemLoadAverageWidget/reducers';
+import memoryStatzReducer from './widgets/MemoryStatzWidget/reducers';
 
-const store = createStore(mozartReducer);
+const rootReducer = combineReducers({
+  global: mozartReducer,
+  networkStatz: networkStatzReducer,
+  cpuSystemLoadAvg: cpuSystemLoadAvgReducer,
+  memoryStatz: memoryStatzReducer,
+});
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
