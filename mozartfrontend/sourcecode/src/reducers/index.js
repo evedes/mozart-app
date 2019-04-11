24b23d3ac17bac63583758df0caaf228347bc6b7
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-04-11 02:07:58 +0100

    imported consts

--- mozartfrontend/sourcecode/src/reducers/index.js
+++ mozartfrontend/sourcecode/src/reducers/index.js
@@ -1,3 +1,10 @@
+import {
+  CHANGE_CHARTING_PERIOD,
+  CHANGE_POLLING_PERIOD,
+  CHANGE_CURRENT_BREAKPOINT,
+  TOGGLE_DASHBOARD_LOCK,
+} from '../constants';
+
 const initialState = {
   chartingPeriod: '10',
   pollingPeriod: '5',
@@ -9,22 +16,22 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   const { type } = action;
   switch (type) {
-    case 'CHANGE_CHARTING_PERIOD':
+    case CHANGE_CHARTING_PERIOD:
       return {
         ...state,
         chartingPeriod: action.chartingPeriod,
       };
-    case 'CHANGE_POLLING_PERIOD':
+    case CHANGE_POLLING_PERIOD:
       return {
         ...state,
         pollingPeriod: action.pollingPeriod,
       };
-    case 'CHANGE_CURRENT_BREAKPOINT':
+    case CHANGE_CURRENT_BREAKPOINT:
       return {
         ...state,
         currentBreakpoint: action.currentBreakpoint,
       };
-    case 'TOGGLE_DASHBOARD_LOCK':
+    case TOGGLE_DASHBOARD_LOCK:
       return {
         ...state,
         isDashboardLocked: !state.isDashboardLocked,
