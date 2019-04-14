8698317283ed28deab2d777d1314f63cca663d2b
Eduardo Vedes <eduardo.vedes@gmail.com>
2019-04-14 16:00:46 +0100

    created to handle socket functions

--- mozartfrontend/sourcecode/src/libs/socketLib.js
+++ mozartfrontend/sourcecode/src/libs/socketLib.js
@@ -2,7 +2,7 @@ import openSocket from 'socket.io-client';
 
 const socket = openSocket('http://mozart_backend:3001');
 
-export const subscribeToNetworkStatz = (cb, interval) => {
-  socket.on('networkStatz', message => cb(message));
-  return socket.emit('subscribeToNetworkStatz', interval);
+export const subscribeToNetworkStatz = (cb, chartingPeriod, pollingPeriod) => {
+  socket.on('networkStatz', networkStatz => cb(networkStatz));
+  return socket.emit('subscribeToNetworkStatz', chartingPeriod, pollingPeriod);
 };
